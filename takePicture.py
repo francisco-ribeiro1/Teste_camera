9007f64e92b522eb1db5ae4f051f92d829ac250d
Francisco Augusto dos Santos Ribeiro <53451290+francisco-ribeiro1@users.noreply.github.com>
2021-02-12 10:06:30 -0300

    Add files via upload

--- takePicture.py
+++ takePicture.py
@@ -1,21 +1,24 @@
-from gpiozero import Button
+#from gpiozero import Button
 from picamera import PiCamera
 from datetime import datetime
 from signal import pause
 from time import sleep
 
-clickButton = Button (6)
+#clickButton = Button (6)
 
 #camera = PiCamera()
 #camera = PiCamera(resolution=(3280, 2464))
 camera = PiCamera()
 print(camera.MAX_RESOLUTION)
 
-def capture():
-    timestamp = datetime.now().isoformat()
-    camera.capture('/home/pi/Pictures/%s.png' % timestamp)
+#def capture():
+#    timestamp = datetime.now().isoformat()
+#    camera.capture('/home/pi/Pictures/%s.png' % timestamp)
 
-clickButton.when_pressed = capture
+timestamp = datetime.now().isoformat()
+camera.capture('/home/pi/Pictures/%s.png' % timestamp)
+
+#clickButton.when_pressed = capture
 
 #sleep(3)
 #capture()
